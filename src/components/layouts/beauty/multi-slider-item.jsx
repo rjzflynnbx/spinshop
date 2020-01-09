dd23ff3920ff8d08c77951f408b7d706e40e28e2
Richard Flynn <richardflynn@Administrators-MacBook-Pro.local>
2020-01-09 12:50:10 +0000

    social proof msg in multi-slider-item

--- src/components/layouts/beauty/multi-slider-item.jsx
+++ src/components/layouts/beauty/multi-slider-item.jsx
@@ -1,12 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Slider from 'react-slick';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
 class MultiSliderItem extends Component {
-    render (){
-        const {items, symbol, NoOfProducts} = this.props;
+
+
+    componentWillMount() {
+        console.log(this.props.items)
+    }
+
+    render() {
+        const { items, symbol, NoOfProducts } = this.props;
+
+        const getPStyle = (itemId) => {
+            if (itemId % 2 == 0) {
+                return redP;
+            } else {
+                return greenP;
+            }
+        }
+
+        const getSocialProofMessage = (itemId) => {
+            if (itemId % 2 == 0) {
+                var randNum = Math.floor(Math.random() * 5) + 1;
+                return "Only " + randNum + " left in stock - order soon.";
+            } else {
+                var randNum = Math.floor(Math.random() * 5) + 1;
+                return randNum + " others are looking at this item";
+            }
+        }
+
+
+
+        
+
+        const divStyle = {
+            marginTop: 30,
+            fontSize: 1
+        };
+
+        const redP = {
+            fontSize: 10,
+            color: 'red'
+        };
+
+        const greenP = {
+            fontSize: 10,
+            color: 'green'
+        };
 
         var arrays = [];
         while (items.length > 0) {
@@ -14,35 +57,42 @@ class MultiSliderItem extends Component {
         }
 
         return (
-                <Slider className="offer-slider slide-1">
-                    {arrays.map((products, index) =>
-                        <div key={index}>
-                            {products.map((product, i) =>
-                                <div className="media" key={i}>
-                                    <Link to={`${process.env.PUBLIC_URL}/no-sidebar/product/${product.id}`}>
-                                        <img className="img-fluid" src={
-                                            product.variants?
-                                                product.variants[0].images
-                                                :product.pictures[0]
-                                        } alt="" />
-                                    </Link>
-                                    <div className="media-body align-self-center">
-                                        <div className="rating">
-                                            <i className="fa fa-star"></i>
-                                            <i className="fa fa-star"></i>
-                                            <i className="fa fa-star"></i>
-                                            <i className="fa fa-star"></i>
-                                            <i className="fa fa-star"></i>
-                                        </div>
-                                        <Link to={`${process.env.PUBLIC_URL}/no-sidebar/product/${product.id}`}><h6>{product.name}</h6></Link>
-                                        <h4>{symbol}{product.price-(product.price*product.discount/100)}
-                                            <del><span className="money">{symbol}{product.price}</span></del></h4>
+            <Slider className="offer-slider slide-1">
+                {arrays.map((products, index) =>
+                    <div key={index}>
+                        {products.map((product, i) =>
+                            <div className="media" key={i}>
+                                <Link to={`${process.env.PUBLIC_URL}/no-sidebar/product/${product.id}`}>
+                                    <img className="img-fluid" src={
+                                        product.variants ?
+                                            product.variants[0].images
+                                            : product.pictures[0]
+                                    } alt="" />
+                                </Link>
+                                <div className="media-body align-self-center">
+                                    <div className="rating">
+                                        <i className="fa fa-star"></i>
+                                        <i className="fa fa-star"></i>
+                                        <i className="fa fa-star"></i>
+                                        <i className="fa fa-star"></i>
+                                        <i className="fa fa-star"></i>
                                     </div>
+                                    <Link to={`${process.env.PUBLIC_URL}/no-sidebar/product/${product.id}`}><h6>{product.name}</h6></Link>
+                                    <h4>{symbol}{product.price - (product.price * product.discount / 100)}
+                                        <del><span className="money">{symbol}{product.price}</span></del></h4>
+
+                                    {/*  */}
+
+                                    <div style={divStyle}>
+                                        <p style={getPStyle(product.id)}>{getSocialProofMessage(product.id)}</p>
+                                    </div>
+
                                 </div>
-                            )}
-                        </div>
-                    )}
-                </Slider>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </Slider>
         )
     }
 }
